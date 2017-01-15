913669fc2bbf01363834d52a96528940f750b279
making <makingx@gmail.com>
2017-01-16 01:42:32 +0900

    add test data

---
+++ src/main/resources/db/migration/mysql/V6.1.0__insert-test-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO conference (conf_id, conf_date, conf_name, conf_status, conf_cfp_note, conf_vote_note)
+VALUES (0x000000000000000000000aaa20170101, '2017-01-01', 'JJUG AAA 2017', 5, 'Cfp for JJUG AAA 2017!!',
+        'Vote for JJUG AAA 2017!!');
+INSERT INTO conference (conf_id, conf_date, conf_name, conf_status, conf_cfp_note, conf_vote_note)
+VALUES (0x000000000000000000000bbb20170102, '2017-01-02', 'JJUG BBB 2017', 5, 'Cfp for JJUG BBB 2017!!',
+        'Vote for JJUG BBB 2017!!');
